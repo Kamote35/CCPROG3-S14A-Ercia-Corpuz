6594bfd09ba980c9297052b1418de0747ecd0aa1
MarcErcia <marc_ercia@dlsu.edu.ph>
2025-07-01 14:31:18 +0800

    Add files via upload

---
+++ MonopolyManila.java
@@ -0,0 +1,11 @@
+public class MonopolyManila
+{
+    public static void main(String[] args)
+    {
+        // Initialize the game
+        Game game = new Game();
+        
+        // Start the game
+        game.start();
+    }   
+}
