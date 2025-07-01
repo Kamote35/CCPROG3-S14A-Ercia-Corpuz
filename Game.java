42c4e967647fa25fefb3124e953659d79a1f60f3
MarcErcia <marc_ercia@dlsu.edu.ph>
2025-07-01 14:42:08 +0800

    Add files via upload
    Added SpecialBlock and PropertyBlock instances to game board in initializeBoard method. Added dice roll method.

--- Game.java
+++ Game.java
@@ -15,6 +15,61 @@ public class Game {
     }
 
     public void initializeBoard() {
-        
+        board.add(new SpecialBlock("GO", 1, "GO"));
+        board.add(new PropertyBlock("Muralla Street", 2, 25000, 5000));
+        board.add(new SpecialBlock("Internal Revenue Allotment", 3, "INTERNAL_REVENUE"));
+        board.add(new SpecialBlock("Income Tax", 4, "INCOME_TAX"));
+        board.add(new SpecialBlock("Meralco", 5, "MERALCO"));
+        board.add(new SpecialBlock("Maynilad", 6, "MAYNILAD"));
+        board.add(new SpecialBlock("Real Property Tax", 7, "REAL_PROPERTY_TAX"));
+        board.add(new SpecialBlock("Manila City Jail", 8, "JAIL"));
+    }
+
+    public void start() {
+        System.out.println("Welcome to the game!");
+        System.out.println("Enter number of players (2-4):");
+        int num = Integer.parseInt(System.console().readLine());
+        for (int i = 0; i < num; i++) {
+            System.out.print("Enter name of Player " + (i + 1) + ": ");
+            String name = System.console().readLine();
+            players.add(new Player(name));
+        }
+
+        boolean gameOver = false;
+
+        while (!gameOver) {
+            for (Player player : players) {
+                System.out.println("\n" + player.name + "'s turn. Cash: ₱" + player.cash);
+
+                int d1 = rollDice();
+                int d2 = rollDice();
+                int move = d1 + d2;
+
+                System.out.println("Rolled: " + d1 + " and " + d2 + " -> Move: " + move);
+
+                player.position += move;
+                if (player.position > board.size()) {
+                    player.position = player.position % board.size();
+                    System.out.println(player.name + " passed GO! +₱2,500");
+                    player.cash += 2500;
+                }
+
+                Block block = board.get(player.position - 1);
+                block.landedOn(player, this);
+
+                System.out.println(player.name + "'s cash: ₱" + player.cash);
+                System.out.println(player.name + "'s net worth: ₱" + player.getNetWorth());
+
+                if (player.cash < 0) {
+                    System.out.println(player.name + " is bankrupt! Game over.");
+                    gameOver = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    public int rollDice() {
+        return rand.nextInt(6) + 1; // returns a number between 1 and 6
     }
 }
